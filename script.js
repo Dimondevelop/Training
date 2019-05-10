c979b0ece1c6256cb1d9b2f3d30a08afdfa0a3c1
Dmytro Petrashchuk <Dimon.develop@gmail.com>
2019-05-10 23:09:36 +0300

    create Save/Delete buttons with colorStatus

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function sayHi() {
 function sayBye() {
     alert('Goodbye from Git!')
 }
+
+window.onload = function (e) {
+    let buttons = document.querySelectorAll('.buttons input');
+
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener('click', btnClick);
+    }
+};
+function btnClick(e) {
+    let status = document.querySelector('#status');
+    let color = this.getAttribute('data-color');
+    // for(var k in status){
+    //     document.body.innerHTML += '<b>' + k + '</b> ' + status[k] + '<br>';
+    // }
+    status.style.backgroundColor = color;
+    console.log();
+
+}
