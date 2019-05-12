6f7c524c1c9298d315af2050e3f802dd119f950d
Dmytro Petrashchuk <Dimon.develop@gmail.com>
2019-05-12 13:57:57 +0300

    slider move to function - sliderTestJS

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var setting = {
+    gallery: '.gallery .img',
+    nextSlide: 'next',
+    prevSlide: 'previous',
+    auto: true,
+    speed: 1000,
+    visibleClass: 'img_active'
+};
+
 window.onload = function () {
     let colorChangers = document.querySelectorAll('.buttons input');
     for (let i = 0; i < colorChangers.length; i++) {
@@ -5,32 +14,29 @@ window.onload = function () {
         colorChangers[i].addEventListener('mouseenter', elementHoverAdd);
         colorChangers[i].addEventListener('mouseleave', elementHoverRemove);
     }
+    sliderTestJS(setting);
+};
 
-    let gallery = document.querySelectorAll('.gallery .img');
-    let previous = document.getElementById('previous');
-    let next = document.getElementById('next');
-
+function sliderTestJS(setting) {
+    let gallery = document.querySelectorAll(setting.gallery);
+    let previous = document.getElementById(setting.prevSlide);
+    let next = document.getElementById(setting.nextSlide);
 
     let i = 0;
-    previous.onclick = function () {
-        gallery[i].classList.remove('img_active');
+    previous.onclick = function (e) {
+        gallery[i].classList.remove(setting.visibleClass);
         if (i===0) i = gallery.length-1;
         else i--;
-        gallery[i].classList.add('img_active');
+        gallery[i].classList.add(setting.visibleClass);
 
     };
-    next.onclick = function () {
-        gallery[i].classList.remove('img_active');
+    next.onclick = function (e) {
+        gallery[i].classList.remove(setting.visibleClass);
         if (i === gallery.length-1) i = 0;
         else i++;
-        gallery[i].classList.add('img_active');
+        gallery[i].classList.add(setting.visibleClass);
     };
-
-
-    gallery.forEach(function (element) {
-
-    })
-};
+}
 
 function colorChange(e) {
     let blockId = this.getAttribute('data-block');
