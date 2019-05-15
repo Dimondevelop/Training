f5d5518c8aa47a029f9784259f273d7b08668528
Dmytro Petrashchuk <Dimon.develop@gmail.com>
2019-05-15 11:29:03 +0300

    Refactor code style

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 window.onload = function () {
+    const setting = {
+        slideStart: 0,
+        prevSlideID: 'previous',
+        nextSlideID: 'next',
+        speed: 4000,
+        visibleClass: 'img_active',
+        radioThumbsID: 'radio-thumbs',
+        reverse: false
+    };
+    new SliderTestJS(setting);
+
     let colorChangers = document.querySelectorAll('.buttons input');
     for (let i = 0; i < colorChangers.length; i++) {
         colorChangers[i].addEventListener('change', colorChange);
@@ -6,6 +17,114 @@ window.onload = function () {
         colorChangers[i].addEventListener('mouseleave', elementHoverRemove);
     }
 };
+
+function SliderTestJS(setting) {
+    //конструктор
+    let slider = this; //SliderTestJS Context
+    let i = slider.slideStart || 0;
+    slider.gallery = setting.gallery ? document.querySelectorAll(setting.gallery) : document.querySelectorAll('.gallery .gallery_img');
+    slider.previous = setting.prevSlideID ? document.getElementById(setting.prevSlideID) : document.getElementById('previous');
+    slider.next = setting.nextSlideID ? document.getElementById(setting.nextSlideID) : document.getElementById('next');
+    slider.radioThumbs = setting.radioThumbsID ? document.getElementById(setting.radioThumbsID) : document.getElementById('radio-thumbs');
+    slider.auto = setting.auto || false;
+    slider.visibleClass = setting.visibleClass || 'img_active';
+    slider.isPause = false;
+    slider.switchTimeOut = setting.switchTimeOut || 5000;
+    if (slider.auto) {
+        slider.speed = setting.speed || 5000;
+        slider.reverse = setting.reverse || false;
+    }
+
+    //переключатель назад
+    slider.slidePrevious = function () {
+        slider.gallery[i].classList.remove(slider.visibleClass);
+        if (i === 0) i = slider.gallery.length - 1;
+        else i--;
+        thumbnailSelect(slider.radioThumbs);
+        slider.gallery[i].classList.add(slider.visibleClass);
+    };
+
+    //переключатель вперел
+    slider.slideNext = function () {
+        slider.gallery[i].classList.remove(slider.visibleClass);
+        if (i === slider.gallery.length - 1) i = 0;
+        else i++;
+        thumbnailSelect(slider.radioThumbs);
+        slider.gallery[i].classList.add(slider.visibleClass);
+    };
+
+    //переключатель по index
+    slider.slideSwitcher = function (index) {
+        slider.gallery[i].classList.remove(slider.visibleClass);
+        i = index;
+        thumbnailSelect(slider.radioThumbs);
+        slider.gallery[i].classList.add(slider.visibleClass);
+    };
+
+    //додати до слайдера переключателі (радіо)
+    slider.thumbnailRadioAdd = function (thumbSelector) {
+        slider.gallery.forEach(function () {
+            thumbSelector.innerHTML += '<input name="thumbnail" type="radio">';
+        });
+        slider.thumbInputs = thumbSelector.querySelectorAll('input');
+        slider.thumbInputs[i].checked = true;
+        slider.thumbInputs.forEach(function (element, index) {
+            element.addEventListener('click', function () {
+                slider.slideSwitcher(index);
+                timeOutId = slider.slideSwitchTimeOut(timeOutId);
+            })
+        })
+    };
+
+    //обрати всі інпути в селекторі thumbInputs та відмічати номер відповідний до номеру показаного слайду
+    function thumbnailSelect(thumbSelector) {
+        let thumbInputs = thumbSelector.querySelectorAll('input');
+        thumbInputs[i].checked = true;
+    }
+    slider.thumbnailRadioAdd(slider.radioThumbs);
+
+    /*f приймає на вхід швидкість автопереключення слайдів та в залежності від обраного користувачем параметру reverse
+    встановлює таймер на функцію переключення слайдеру. Автопереключення відбувається тільки якщо не пауза.
+    Повертає ID встановленого таймеру*/
+    slider.setTimer = function (speed) {
+        let timerId = 0;
+        if (slider.auto && slider.reverse) {
+            timerId = setInterval(() => {
+                if (!slider.isPause) slider.slidePrevious();
+            }, speed);
+        } else if (slider.auto) {
+            timerId = setInterval(() => {
+                if (!slider.isPause) slider.slideNext();
+            }, speed);
+        }
+        return timerId;
+    };
+
+    /*міняє значення isPause на true (ставить на паузу автопереключення слайдів) та
+    ставить таймер на обраний час (за замовчуванням 5 сек), після чого його назад на false.
+    Якщо таймер вже був встановлений, спочатку видаляється старий таймер, після чого встановлюється новий.
+    повертає id встановленого таймеру*/
+    slider.slideSwitchTimeOut = function (timeOutId = 0) {
+        slider.isPause = true;
+        if (timeOutId !== 0) clearTimeout(timeOutId);
+        timeOutId = setTimeout(function () {
+            slider.isPause = false;
+        }, slider.switchTimeOut);
+        return timeOutId;
+    };
+
+    slider.setTimer(slider.speed);
+    
+    let timeOutId = 0;
+    previous.onclick = () => {
+        slider.slidePrevious();
+        timeOutId = slider.slideSwitchTimeOut(timeOutId);
+    };
+    next.onclick = () => {
+        slider.slideNext();
+        timeOutId = slider.slideSwitchTimeOut(timeOutId);
+    };
+}
 
 function colorChange() {
     let blockId = this.getAttribute('data-block');
@@ -23,95 +142,4 @@ function elementHoverRemove() {
     let blockId = this.getAttribute('data-block');
     let block = document.getElementById(blockId);
     block.classList.remove('block-hover');
-}
-
-function sliderTestJS(setting) {
-    let i = this.slideStart || 0;
-    let gallery = document.querySelectorAll(setting.gallery) || document.querySelectorAll('.gallery .gallery_img');
-    let previous = document.getElementById(setting.prevSlideID) || document.getElementById('previous');
-    let next = document.getElementById(setting.nextSlideID) || document.getElementById('next');
-    let radioThumbs = document.getElementById(setting.radioThumbsID) || document.getElementById('radio-thumbs');
-    let isPause = false;
-    if (setting.auto) {
-        this.speed = setting.speed || 5000;
-        this.reverse = setting.reverse || false;
-    }
-    let slidePrevious = function () {
-        gallery[i].classList.remove(setting.visibleClass);
-        if (i === 0) i = gallery.length - 1;
-        else i--;
-        thumbnailSelect(radioThumbs);
-        gallery[i].classList.add(setting.visibleClass);
-    };
-
-    let slideNext = function () {
-        gallery[i].classList.remove(setting.visibleClass);
-        if (i === gallery.length - 1) i = 0;
-        else i++;
-        thumbnailSelect(radioThumbs);
-        gallery[i].classList.add(setting.visibleClass);
-    };
-
-    thumbnailRadioAdd(radioThumbs);
-
-    function setTimer(speed) {
-        let timerId = 0;
-        if (setting.auto && setting.reverse) {
-            timerId = setInterval(() => {
-                if (!isPause) slidePrevious();
-            }, speed);
-        } else if (setting.auto) {
-            timerId = setInterval(() => {
-                if (!isPause) slideNext();
-            }, speed);
-        }
-        return timerId;
-    }
-
-    function slideSwitchTimeOut(timeOutId = 0) {
-        isPause = true;
-        if (timeOutId !== 0) clearTimeout(timeOutId);
-        timeOutId = setTimeout(function () {
-            isPause = false;
-        }, 5000);
-        return timeOutId;
-    }
-
-    setTimer(this.speed);
-    let timeOutId = 0;
-    previous.onclick = () => {
-        slidePrevious();
-        timeOutId = slideSwitchTimeOut(timeOutId);
-    };
-    next.onclick = () => {
-        slideNext();
-        timeOutId = slideSwitchTimeOut(timeOutId);
-    };
-
-    function thumbnailRadioAdd(thumbSelector) {
-        gallery.forEach(function () {
-            thumbSelector.innerHTML += '<input name="thumbnail" type="radio">';
-        });
-
-        let thumbInputs = thumbSelector.querySelectorAll('input');
-        thumbInputs[i].checked = true;
-        thumbInputs.forEach(function (element, index) {
-            element.addEventListener('click',function () {
-                slideSwitcher(index)
-                timeOutId = slideSwitchTimeOut(timeOutId);
-            })
-        })
-    }
-
-    function slideSwitcher(index) {
-        gallery[i].classList.remove(setting.visibleClass);
-        i = index;
-        thumbnailSelect(radioThumbs);
-        gallery[i].classList.add(setting.visibleClass);
-    }
-
-    function thumbnailSelect(thumbSelector){
-        let thumbInputs = thumbSelector.querySelectorAll('input');
-        thumbInputs[i].checked = true;
-    }
 }
